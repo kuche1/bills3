5a32da0861bbb7362ca5dae1849e2bdcb9d036a9
kuche1 <znamsiparolata4esno@abv.bg>
2025-07-09 01:47:04 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,20 @@ fn main() {
 
     let (year, month) = date::get();
 
+    let mut money = 0.0;
+
     let (income, expenditures) = toml::read(&data_folder, year, month);
+
+    money += income;
+
+    for (day_idx, day_expenditure) in expenditures.iter().enumerate() {
+        let day = day_idx + 1;
+
+        money -= day_expenditure;
+
+        println!(
+            "{:02}.{:02} -> {:5.2} -> {:.2}",
+            month, day, day_expenditure, money
+        );
+    }
 }
